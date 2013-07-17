d3bb078032ad3fa6b52d31a1a343ad6aa46a43ad
Tim Smith <tsmith@bfgcom.com>
2013-07-17 11:36:38 -0400

    fixed  file requirement issues with correct folder paths

--- pixels/expressionengine/config/database.php
+++ pixels/expressionengine/config/database.php
@@ -3,10 +3,10 @@
 $active_group = 'expressionengine';
 $active_record = TRUE;
 
-$db['expressionengine']['hostname'] = 'localhost';
-$db['expressionengine']['username'] = 'root';
-$db['expressionengine']['password'] = 'root';
-$db['expressionengine']['database'] = 'dev_pixelsandcode';
+// $db['expressionengine']['hostname'] = 'localhost';
+// $db['expressionengine']['username'] = 'root';
+// $db['expressionengine']['password'] = 'root';
+// $db['expressionengine']['database'] = 'dev_pixelsandcode';
 $db['expressionengine']['dbdriver'] = 'mysql';
 $db['expressionengine']['pconnect'] = FALSE;
 $db['expressionengine']['dbprefix'] = 'exp_';
@@ -16,7 +16,10 @@ $db['expressionengine']['cache_on'] = FALSE;
 $db['expressionengine']['autoinit'] = FALSE;
 $db['expressionengine']['char_set'] = 'utf8';
 $db['expressionengine']['dbcollat'] = 'utf8_general_ci';
-$db['expressionengine']['cachedir'] = '/Applications/MAMP/Development/ExpressionEngine/pixelsandcode/pixels/expressionengine/cache/db_cache/';
+// $db['expressionengine']['cachedir'] = '/Applications/MAMP/Development/ExpressionEngine/pixelsandcode/pixels/expressionengine/cache/db_cache/';
+// 
+
+require $_SERVER['DOCUMENT_ROOT'] . '/config/config.master.php';
 
 /* End of file database.php */
 /* Location: ./system/expressionengine/config/database.php */
